1a454641af3a3034aaca4a147c94c6c25435bd26
aibazhang <dtjp@tutanota.com>
2021-06-15 22:43:30 +0900

    remove inkeyborad example

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,15 +1,15 @@
 from bighea.news.collector import NewsAPICollector
 import pytest
 
-nac = NewsAPICollector(
-    country="jp",
-    category="general",
-    page_size=10,
-    print_format="markdown",
-    mode="top-headlines",
-)
+# nac = NewsAPICollector(
+#     country="jp",
+#     category="general",
+#     page_size=10,
+#     print_format="markdown",
+#     mode="top-headlines",
+# )
 
-nac.collcet_news()
+# nac.collcet_news()
 
 
 @pytest.mark.parametrize("x, y", [("aaa", "bbb"), ("aaa", "aaa"), ("bbb", "bbb")])
